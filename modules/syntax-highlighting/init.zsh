5b9662011d05b5e7d2c972a9c78948e2894f82ac
Thales Mello <thalesmello@gmail.com>
2016-07-21 03:24:00 -0300

    Remore custom from git
    Load syntax highlighting only once

--- modules/syntax-highlighting/init.zsh
+++ modules/syntax-highlighting/init.zsh
@@ -5,4 +5,6 @@
 # highlighters
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(${zhighlighters[@]})
 
-source "${0:h}/external/zsh-syntax-highlighting.zsh" || return 1
+if [ -z $ZSH_HIGHLIGHT_REVISION ]; then
+  source "${0:h}/external/zsh-syntax-highlighting.zsh" || return 1
+fi
